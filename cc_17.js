0aecb2474530204f9d652bfdcabaec3e24d37319
cstone45 <cstone45@usf.edu>
2025-03-30 07:46:47 -0400

    Created Sales Rep Class

--- cc_17.js
+++ cc_17.js
@@ -19,3 +19,24 @@ class Customer {
 const jeff = new Customer("Jeff", "Jeff@biz.com", [500, 300, 100]);
 console.log(jeff.getDetails());
 console.log(jeff.getTotalSpent());
+
+//Task 2: Create a Sales Rep Class
+class SalesRep {
+    constructor(name, clients) {
+        this.name = name;
+        this.clients = clients;
+    }
+    addClient(customer) {
+        this.clients.push(customer);
+    }
+    getClientTotal(name) {
+        for(let i=0; i<this.clients.length; i++){
+            if(this.clients[i].name===name){
+                return this.clients[i].getTotalSpent();
+            }
+        }
+    }
+};
+const tracy = new SalesRep("tracy", [jeff, new Customer("Jim", "Jim@fun.com", [300, 200, 50])]);
+console.log(tracy);
+console.log(tracy.getClientTotal("Jim"));
